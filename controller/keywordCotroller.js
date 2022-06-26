9072d6a47f60896ea1459ec20ce0d7db64123d25
DESKTOP-GEFTSHN\ADMIN <tqtteams1st@gmail.com>
2022-06-26 16:18:51 +0700

    Update keyword controller

--- controller/keywordCotroller.js
+++ controller/keywordCotroller.js
@@ -9,10 +9,10 @@ const keywordController = {
         try {
             const keyword = new Keyword(req.body);
             const saveKeyword = await keyword.save();
-            if(req.body.user){
-                const user = User.find({id: req.body.user});
-                await user.updateOne({$push: {keywords:saveKeyword.id}})
-            }
+            // if(req.body.user){
+            //     const user = User.find({id: req.body.user});
+            //     await user.updateOne({$push: {keywords:saveKeyword.id}})
+            // }
             res.status(200).json(keyword);
         } catch (error) {
             res.status(500).json(error);
